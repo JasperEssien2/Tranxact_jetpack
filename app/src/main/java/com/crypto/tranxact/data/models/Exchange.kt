093b2df0d1857c44b7d2e57af8921c22a64b4188
JasperEssien2 <jasperessien2@gmail.com>
2022-06-03 00:53:11 +0100

    added necessary files

--- app/src/main/java/com/crypto/tranxact/data/models/Exchange.kt
+++
@@ -1,8 +0,0 @@
-package com.crypto.tranxact.data.models
-
-import com.google.gson.annotations.SerializedName
-
-data class Exchange(
-    @SerializedName("exchange_id") val id: String,
-    @SerializedName("name") val name: String,
-)
